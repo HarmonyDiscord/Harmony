a76ea8996cafc9030c1f23fddf774f4cfc8f1c35
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-07-13 01:37:42 +0100

    add controls & better navbar

---
+++ src/components/layout/Controls.tsx
@@ -0,0 +1,45 @@
+import { Box, Text, Flex, Image, Heading, IconButton, Spacer } from '@chakra-ui/react';
+import { MdLoop, MdPlayArrow, MdShuffle, MdSkipNext, MdSkipPrevious, MdVolumeUp } from 'react-icons/md';
+
+export default function Controls() {
+	return (
+		<Box w='100%' p='20px' pt='0px'>
+			<Flex
+				w='100%'
+				bg='#FFFFFF10'
+				borderRadius='10px'
+				p='20px'
+				gap='5px'
+				zIndex={2}
+				alignItems='center'
+				backdropFilter='blur(5px)'
+			>
+				<Flex gap='12px' alignItems='center'>
+					<Image
+						width='48px'
+						height='48px'
+						src='https://cdn.discordapp.com/attachments/1245099205337415811/1261445572947345538/image.png?ex=6692fc3d&is=6691aabd&hm=2d336793acf4819c171cad086fe4d4fdf7fa954304efe2c5d6af678eeabc2625&'
+						alt='si'
+						objectFit='cover'
+					/>
+					<Flex gap='4px' direction='column'>
+						<Heading size='md'>La Mamada</Heading>
+						<Text>Mamadas – Leiva</Text>
+					</Flex>
+				</Flex>
+				<Spacer />
+				<Flex gap='10px'>
+					<IconButton icon={<MdSkipPrevious fontSize='24px' />} aria-label='Previous' />
+					<IconButton icon={<MdPlayArrow fontSize='24px' />} aria-label='Play' />
+					<IconButton icon={<MdSkipNext fontSize='24px' />} aria-label='Next' />
+				</Flex>
+				<Spacer />
+				<Flex gap='10px'>
+					<IconButton icon={<MdVolumeUp fontSize='24px' />} aria-label='Previous' />
+					<IconButton icon={<MdLoop fontSize='24px' />} aria-label='Play' />
+					<IconButton icon={<MdShuffle fontSize='24px' />} aria-label='Next' />
+				</Flex>
+			</Flex>
+		</Box>
+	);
+}
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,36 @@
+import { Avatar, Box, Flex, Heading, Input, InputGroup, InputLeftElement, Spacer } from '@chakra-ui/react';
+import { useAtom } from 'jotai';
+import { MdMusicNote, MdSearch } from 'react-icons/md';
+import { userAtom } from 'src/atoms/UserAtom';
+
+export default function Navbar() {
+	const [user] = useAtom(userAtom);
+
+	return (
+		<Box w='100%' p='20px' pb='0px'>
+			<Flex
+				w='100%'
+				bg='#FFFFFF10'
+				borderRadius='10px'
+				p='20px'
+				gap='10px'
+				zIndex={2}
+				alignItems='center'
+				backdropFilter='blur(5px)'
+			>
+				<MdMusicNote fontSize='36px' color='#FFFFFF' />
+				<Heading size='md' fontWeight='bold'>
+					Harmony
+				</Heading>
+				<Spacer />
+				<InputGroup w='400px'>
+					<InputLeftElement pointerEvents='none'>
+						<MdSearch fontSize='22px' />
+					</InputLeftElement>
+					<Input variant='filled' focusBorderColor='#FFFFFF30' placeholder='Search for a song...' />
+				</InputGroup>
+				<Avatar width='40px' height='40px' src={user?.avatarURL} />
+			</Flex>
+		</Box>
+	);
+}

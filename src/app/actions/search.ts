fe9c2f289ebb67c18aa37ef95275c33dd5da3243
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-07-13 23:01:22 +0100

    improve song selection

--- src/app/actions/search.ts
+++ src/app/actions/search.ts
@@ -29,14 +29,28 @@ export default async function search(query: string) {
 	}
 }
 
+function formatDuration(duration: number) {
+	const hr = Math.floor(duration / 3600);
+	const mr = Math.floor((duration % 3600) / 60);
+	const sr = duration % 60;
+
+	return (
+		(hr > 0 ? hr.toString().padStart(2, '0') + ':' : '') +
+		mr.toString().padStart(2, '0') +
+		':' +
+		sr.toString().padStart(2, '0')
+	);
+}
+
 function parseSearchResult(results: SongDetailed): Song {
+
 	return {
 		id: results.videoId,
 		title: results.name,
 		artist: results.artist.name,
 		album: results.album?.name || 'unknown',
 		cover: results.thumbnails.at(0)?.url.replace('w60', 'w250').replace('h60', 'h250'),
-		duration: results.duration?.toString() || '0'
+		duration: formatDuration(results.duration ?? 0)
 	};
 }
 

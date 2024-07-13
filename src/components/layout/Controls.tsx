747344c0141988eaa302a652af7444e49803c758
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-07-13 23:23:04 +0100

    song controls

--- src/components/layout/Controls.tsx
+++ src/components/layout/Controls.tsx
@@ -1,45 +1,72 @@
 import { Box, Flex, Heading, IconButton, Image, Spacer, Text } from '@chakra-ui/react';
-import { MdLoop, MdPlayArrow, MdShuffle, MdSkipNext, MdSkipPrevious, MdVolumeUp } from 'react-icons/md';
+import { useAtom } from 'jotai';
+import { MdClose, MdLoop, MdPlayArrow, MdSkipNext, MdSkipPrevious, MdVolumeUp } from 'react-icons/md';
+import { currentSongAtom } from '../../atoms/CurrentSongAtom';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Controls() {
+	const [currentSong, setCurrentSong] = useAtom(currentSongAtom);
+
 	return (
-		<Box w='100%' p='20px' pt='0px'>
-			<Flex
-				w='100%'
-				bg='#FFFFFF10'
-				borderRadius='10px'
-				p='20px'
-				gap='5px'
-				zIndex={2}
-				alignItems='center'
-				backdropFilter='blur(5px)'
-			>
-				<Flex gap='12px' alignItems='center'>
-					<Image
-						width='48px'
-						height='48px'
-						src='https://cdn.discordapp.com/attachments/1245099205337415811/1261445572947345538/image.png?ex=6692fc3d&is=6691aabd&hm=2d336793acf4819c171cad086fe4d4fdf7fa954304efe2c5d6af678eeabc2625&'
-						alt='si'
-						objectFit='cover'
-					/>
-					<Flex gap='4px' direction='column'>
-						<Heading size='md'>La Mamada</Heading>
-						<Text>Mamadas – Leiva</Text>
+		<AnimatePresence mode='popLayout'>
+			{currentSong && (
+				<Box
+					as={motion.div}
+					w='100%'
+					p='20px'
+					pt='0px'
+					initial={{ y: 10, opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					exit={{ y: 10, opacity: 0 }}
+				>
+					<Flex
+						w='100%'
+						bg='#FFFFFF10'
+						borderRadius='10px'
+						p='20px'
+						gap='5px'
+						zIndex={2}
+						alignItems='center'
+						backdropFilter='blur(5px)'
+					>
+						<Flex gap='12px' alignItems='center'>
+							<Image
+								width='48px'
+								height='48px'
+								src={currentSong.cover}
+								alt='si'
+								objectFit='cover'
+								style={{
+									borderRadius: '5px'
+								}}
+							/>
+							<Flex gap='4px' direction='column'>
+								<Heading size='md'>{currentSong.title}</Heading>
+								<Text>
+									{currentSong.album} – {currentSong.artist}
+								</Text>
+							</Flex>
+						</Flex>
+						<Flex gap='10px' w='100%' position='absolute'>
+							<Spacer />
+							<IconButton icon={<MdSkipPrevious fontSize='24px' />} aria-label='Previous' />
+							<IconButton icon={<MdPlayArrow fontSize='24px' />} aria-label='Play' />
+							<IconButton icon={<MdSkipNext fontSize='24px' />} aria-label='Next' />
+							<Spacer />
+						</Flex>
+						<Spacer />
+						<Flex gap='10px'>
+							<IconButton icon={<MdVolumeUp fontSize='24px' />} aria-label='Previous' />
+							<IconButton icon={<MdLoop fontSize='24px' />} aria-label='Play' />
+							<IconButton
+								icon={<MdClose fontSize='24px' />}
+								aria-label='Close'
+								onClick={() => setCurrentSong(null)}
+							/>
+						</Flex>
 					</Flex>
-				</Flex>
-				<Spacer />
-				<Flex gap='10px'>
-					<IconButton icon={<MdSkipPrevious fontSize='24px' />} aria-label='Previous' />
-					<IconButton icon={<MdPlayArrow fontSize='24px' />} aria-label='Play' />
-					<IconButton icon={<MdSkipNext fontSize='24px' />} aria-label='Next' />
-				</Flex>
-				<Spacer />
-				<Flex gap='10px'>
-					<IconButton icon={<MdVolumeUp fontSize='24px' />} aria-label='Previous' />
-					<IconButton icon={<MdLoop fontSize='24px' />} aria-label='Play' />
-					<IconButton icon={<MdShuffle fontSize='24px' />} aria-label='Next' />
-				</Flex>
-			</Flex>
-		</Box>
+				</Box>
+			)}
+		</AnimatePresence>
 	);
 }

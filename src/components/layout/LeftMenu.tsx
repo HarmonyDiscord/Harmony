ee6e0502b71b191d1de974887ece5e8718999901
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-07-14 23:41:21 +0100

    fix

--- src/components/layout/LeftMenu.tsx
+++ src/components/layout/LeftMenu.tsx
@@ -60,7 +60,7 @@ export default memo(function LeftMenu() {
 				<Box
 					as={motion.div}
 					p='20px'
-					pl='10px'
+					pl={['20px', '20px', '10px']}
 					h='100%'
 					w={['100%', '100%', '500px']}
 					minW={['100%', '100%', '500px']}
